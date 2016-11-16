03b3656cd9fd43fcc7516f0aa64131f4d8806784
Tim Buckley <timothy.jas.buckley@hpe.com>
2016-11-16 14:31:51 -0700

    Add select and nodeport subcommands.

--- README.md
+++ README.md
@@ -19,7 +19,11 @@ You can update by running `git pull` in the checked out `kc` directory.
 
 Usage
 -----
-Add a `.kc.yml` containing something like the following:
+
+### Directory configuration
+
+Add a `.kc.yml` in your project directory containing something like the
+following:
 
 ```yaml
 namespace: my-namespace
@@ -38,3 +42,54 @@ are passed to `kubectl`, but will be adjusted based on values in `.kc.yml`.
 Examples:
  * `kubectl get pods -n my-namespace` becomes `kc get pods`
  * `kubectl --kubeconfig /path/to/some/config.conf ...` becomes `kc ...`
+
+By default the environment for all `kubectl` commands is inherited. If desired,
+this can be disabled by setting `inherit: false` in `.kc.yml`.
+
+### Environment configuration
+
+Most options can also be configured using environment variables. If a `.kc.yml`
+exists, settings configured in environment variables will override any in the
+config file.
+
+ * `KC_NAMESPACE`: if set, automatically appends `-n $KC_NAMESPACE` to all
+   generated `kubectl` commands
+
+Some common environment variables can also be overridden. If set, these will
+override any values configured in `.kc.yml`, or if `inherit: false` is set, will
+be passed to the `kubectl` child process:
+
+ * `KC_KUBECONFIG`
+ * `KC_HTTP_PROXY`
+ * `KC_HTTPS_PROXY`
+ * `KC_NO_PROXY`
+
+### Helper commands
+
+In addition to wrapping all standard `kubectl` commands, `kc` also provides a
+few extra subcommands that make common operations a bit easier:
+
+ * `kc select` (aka `kc sel`, `kc s`): returns a space-separated list of full
+   resource names given a set of labels. For example:
+       $ kc select app=grafana
+       grafana-1548781338-gjlps
+   This can be used to easily run commands on resources whose names may change,
+   for example:
+       $ kc logs -f $(kc select app=grafana)
+       $ kc exec -it $(kc select app=grafana) bash
+
+   Note that by default the resource is `pod` but that can be overridden with
+   `-r <type>`. See also: `kc select --help`.
+
+ * `kc nodeport` (aka `kc np`): returns a plain integer port for a service
+   NodePort, useful for finding where a randomly-allocated port can be accessed.
+   Example:
+       $ kc nodeport grafana
+       31577
+
+   If multiple ports are exposed, the first is returned. This can be overridden
+   by specifying an integer index (e.g. `kc nodeport grafana 1`) or a port name
+   (e.g. `kc nodeport grafana http`).
+
+A full list of supported sub-commands is available when running `kc help` (it
+will be printed at the bottom of the existing `kubectl` help message).
--- kc.py
+++ kc.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python
 
-import argparse
 import logging
 import os
 import subprocess
 import sys
+
+from argparse import ArgumentParser
+from functools import wraps
 
 import yaml
 
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
+
+verbs = []
+
+
+def verb(name, aliases=None, description=None):
+    if not aliases:
+        aliases = []
+
+    def verb_decorator(func):
+        verbs.append({
+            'name': name,
+            'aliases': aliases,
+            'description': description,
+            'function': func
+        })
+
+        @wraps(func)
+        def func_wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return func_wrapper
+
+    return verb_decorator
 
 
 def resolve_config(from_dir=None):
@@ -64,17 +89,114 @@ def load_config():
     return config
 
 
-def prepare_command(config, user_args):
+def get_environment(config):
+    if config.get('inherit_env', True) is True:
+        env = os.environ.copy()
+    else:
+        env = {}
+
+    env.update(config['env'])
+    return env
+
+
+@verb('select',
+      aliases=['sel', 's'],
+      description='Find an exact resource name based on labels')
+def handle_select(config, remaining_args):
+    parser = ArgumentParser(prog='kc select',
+                            description='Finds an exact resource '
+                                        'name based on labels')
+    parser.add_argument('--resource', '-r', default='pod',
+                        help='The resource type (default: pod)')
+    parser.add_argument('selectors', nargs='+',
+                        help='One or more key=value selectors, space-separated')
+    args = parser.parse_args(remaining_args)
+
+    cmd_path = config.get('kubectl_path', 'kubectl')
+    cmd = [cmd_path]
+    if 'namespace' in config:
+        cmd.extend(['-n', config['namespace']])
+
+    cmd.extend(['get', args.resource, '-l', ','.join(args.selectors)])
+    cmd.append('-o=jsonpath={.items[*].metadata.name}')
+
+    p = subprocess.Popen(cmd, env=get_environment(config))
+    return p.wait()
+
+
+@verb('nodeport', aliases=['np'], description='Resolve a service nodeport')
+def handle_nodeport(config, remaining_args):
+    parser = ArgumentParser(prog='kc nodeport',
+                            description='Resolves a service nodeport')
+    parser.add_argument('service_name',
+                        help='The name of the service to query.')
+    parser.add_argument('port', nargs='?', default='0',
+                        help='The port name or index (default: 0)')
+    args = parser.parse_args(remaining_args)
+
+    cmd_path = config.get('kubectl_path', 'kubectl')
+    cmd = [cmd_path]
+    if 'namespace' in config:
+        cmd.extend(['-n', config['namespace']])
+
+    cmd.extend(['get', 'service', args.service_name])
+
+    try:
+        port = int(args.port)
+        cmd.append('-o=jsonpath={.spec.ports[%d].nodePort}' % port)
+    except ValueError:
+        cmd.append('-o=jsonpath={.spec.ports[?(@.name=="%s")].nodePort}' % args.port)
+
+    p = subprocess.Popen(cmd, env=get_environment(config))
+    return p.wait()
+
+
+#@verb('update', aliases=['up'], description='Updates a configmap in-place')
+def handle_update_configmap(config, remaining_args):
+    print 'TODO'
+    return True
+
+
+#@verb('bash', aliases=['sh'], description='Open an interactive terminal in a pod')
+def handle_bash(config, remaining_args):
+    print 'TODO'
+    return True
+
+
+def handle_special(config, name, remaining_args):
+    verb_defs = filter(lambda v: name == v['name'] or name in v['aliases'], verbs)
+    if verb_defs:
+        return verb_defs[0]['function'](config, remaining_args)
+    else:
+        return None
+
+
+def handle_passthrough(config, user_args):
     cmd_path = config.get('kubectl_path', 'kubectl')
     args = [cmd_path]
-    env = config['env']
 
     if 'namespace' in config:
         args.extend(['-n', config['namespace']])
 
     args.extend(user_args)
 
-    return args, env
+    p = subprocess.Popen(args, env=get_environment(config))
+    return p.wait()
+
+
+def print_kc_help():
+    print '-----'
+    print 'kc wraps kubectl to provide extra functionality.'
+    print ''
+
+    print 'Additional Commands:'
+    for verb_def in verbs:
+        if len(verb_def['aliases']) > 0:
+            aka = ' (aka %s)' % ','.join(verb_def['aliases'])
+        else:
+            aka = ''
+
+        print '  %-15s%s%s' % (verb_def['name'], verb_def['description'] or '', aka)
 
 
 def main():
@@ -82,15 +204,18 @@ def main():
         logger.setLevel(logging.DEBUG)
 
     config = load_config()
-    args, env = prepare_command(config, sys.argv[1:])
+    user_args = sys.argv[1:]
 
-    if config.get('inherit_env', True) is True:
-        env_ = os.environ.copy()
-        env_.update(env)
-        env = env_
+    if not user_args or user_args[0] in ('help', '--help', '-h'):
+        handle_passthrough(config, user_args)
+        print_kc_help()
+        sys.exit(0)
 
-    p = subprocess.Popen(args, env=env)
-    p.wait()
+    special_ret = handle_special(config, user_args[0], user_args[1:])
+    if special_ret is None:
+        sys.exit(handle_passthrough(config, user_args))
+    else:
+        sys.exit(special_ret)
 
 
 if __name__ == '__main__':
